097844d36634b935acc9d801dcebd7af2a7fd4c3
gallangsdw <gallang1800016063@webmail.uad.ac.id>
2020-07-08 10:54:41 +0700

    updated on register

--- app/Views/register.php
+++ app/Views/register.php
@@ -44,7 +44,6 @@
             </div>
           <?php endif; ?>
           </div>
-
           <div class="row">
             <div class="col-12 col-sm-4">
               <button type="submit" class="btn btn-primary">Register</button>
